84cc91f25beda43058277033a1d4b79a3c51fb04
Shruti <shruti@moviequ.com>
2020-12-10 14:44:47 +0530

    4

--- 4_fibonacci .py
+++ 4_fibonacci .py
@@ -3,19 +3,16 @@
 calculated={}
 
 def fibonaci(n):
-    
     if n==0:
         return 0
-    
     elif n==1:
         return 1
     
     elif n in calculated:
+        print(calculated[n])
         return calculated[n]
-    
     else:
         calculated[n]=fibonaci(n-1)+fibonaci(n-2)
         return calculated[n]
-
 print(fibonaci(10))
     
